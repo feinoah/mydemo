c861d2a9ffe12f6caf3ad432bd897b614d2e960a
jsbfec <jsbfec@jsbfecdeiMac.local>
2018-03-09 15:15:59 +0800

    xx

--- node/imooc/views/models/movie.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by ifeng on 2018/1/9.
- */
-var mongoose=require('mongoose')
-var MovieSchema=require('../schemas/movie')
-var Movie=mongoose.model('Movie',MovieSchema)
-module.exports=Movie
